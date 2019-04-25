ed52086508f2d22895ddcc4bdf71d81d39ebd637
Nadine <NadineKraft@gmx.de>
2019-04-25 10:37:44 +0200

    Added footer,header and navbar.php used them to change view files

--- src/view/product.php
+++ src/view/product.php
@@ -1,46 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet"
-          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-            integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
-            crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js"
-            integrity="sha384-xrRywqdh3PHs8keKZN+8zzc5TX0GRTLCcmivcbNJWm2rs5C8PRhcEn3czEjhAO9o"
-            crossorigin="anonymous"></script>
-    <title>Webshop Product</title>
-</head>
-<body>
-<div class="container">
-
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">Webshop</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Products</a>
-                </li>
-            </ul>
-
-            <form class="form-inline my-2 my-lg-0">
-                <button class="btn btn-outline-secondary" type="button">Shopping Cart</button>
-                <button class="btn btn-outline-success" type="button">Suscribe / Login</button>
-            </form>
-
-        </div>
-
-    </nav>
-
+<?php
+$pageTitle = 'Product';
+require('parts/header.php');
+?>
 
     <div class="card mb-3" style="max-width: 540px;">
         <div class="row no-gutters">
@@ -72,5 +33,4 @@
 
 
 </div>
-</body>
-</html>
+<?php require('parts/footer.php');?>
