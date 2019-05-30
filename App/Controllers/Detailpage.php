806133401350ddaaf687c8f3fed5bfbe91e72056
B.Setzer <bennetsetzer@gmx.de>
2019-05-30 21:51:30 +0200

    improved Design(maybe)

--- App/Controllers/Detailpage.php
+++ App/Controllers/Detailpage.php
@@ -14,7 +14,7 @@ class Detailpage extends Controller
 {
     protected  $route_params = null;
     protected $item ;
-    public function ShowDetail()
+    public function showDetail()
     {
         $this->item = Item::getItemByID($this->route_params['id']);
         View::renderTemplate('detailpage.html',["Item" => $this->item]);
